1e549c181b1079fba86fbed4252263863f0c8fae
sakib963 <sakib.celloscope@gmail.com>
2026-09-17 12:00:35 +0600

    refactor: remove legacy copy, data, format, and money computation modules

--- src/app/(home)/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { SYSTEM } from '@/lib/copy';
-
-export default function Loading() {
-  return (
-    <div className="py-24 text-center">
-      <p className="text-lg text-turf-700">{SYSTEM.loaders[0]}</p>
-    </div>
-  );
-}
